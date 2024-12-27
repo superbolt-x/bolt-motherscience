5ed42d11e2089435228c52412fc753488f6a2eb5
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-12-27 10:50:39 -0600

    Update blended_performance.sql
    shopify

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -3,26 +3,29 @@
 )}}
 
 WITH paid_data as
-    (SELECT channel, date::date, date_granularity, COALESCE(SUM(spend),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, 
-        COALESCE(SUM(add_to_cart),0) as add_to_cart, COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(revenue),0) as revenue
+    (SELECT channel, date::date, date_granularity, campaign_name, COALESCE(SUM(spend),0) as spend, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(clicks),0) as clicks, 
+        COALESCE(SUM(add_to_cart),0) as add_to_cart, COALESCE(SUM(purchases),0) as paid_purchases, COALESCE(SUM(revenue),0) as paid_revenue, 
+        0 as sho_purchases, 0 as sho_revenue, 0 as sho_first_orders, 0 as sho_first_order_revenue, 0 as sho_repeat_orders, 0 as sho_repeat_order_revenue
     FROM
-        (SELECT 'Meta' as channel, date, date_granularity, 
+        (SELECT 'Meta' as channel, date, date_granularity, campaign_name,
             spend, impressions, link_clicks as clicks, add_to_cart, purchases, revenue
         FROM {{ source('reporting','facebook_ad_performance') }}
         UNION ALL
-        SELECT 'Google Ads' as channel, date, date_granularity,
+        SELECT 'Google Ads' as channel, date, date_granularity, campaign_name,
             spend, impressions, clicks, null as add_to_cart, purchases, revenue
         FROM {{ source('reporting','googleads_campaign_performance') }}
         )
-    GROUP BY channel, date, date_granularity)
+    GROUP BY channel, date, date_granularity, campaign_name),
+
+    shopify_data as
+    (SELECT 'Shopify' as channel, date, date_granularity, null as campaign_name, 
+        0 as spend, 0 as impressions, 0 as clicks, 0 as add_to_cart, 0 as paid_purchases, 0 as paid_revenue,
+        COALESCE(SUM(orders),0) as sho_purchases, COALESCE(SUM(gross_sales),0) as sho_revenue, 
+        COALESCE(SUM(first_orders),0) as sho_first_orders, COALESCE(SUM(first_order_gross_sales),0) as sho_first_order_revenue, 
+        COALESCE(SUM(repeat_orders),0) as sho_repeat_orders, COALESCE(SUM(repeat_order_gross_sales),0) as sho_repeat_order_revenue
+    FROM {{ source('reporting','shopify_sales') }}
+    GROUP BY channel, date, date_granularity, campaign_name)
     
-SELECT channel,
-    date,
-    date_granularity,
-    spend,
-    impressions,
-    add_to_cart,
-    clicks,
-    purchases,
-    revenue
-FROM paid_data
+SELECT * FROM paid_data
+UNION ALL
+SELECT * FROM shopify_data
