9b8132a643ff1d1f09856abe9ac5fc6989cd95dd
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-02-10 11:46:24 -0600

    Update blended_performance.sql
    sho_net_revenue

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -5,7 +5,7 @@
 WITH paid_data as
     (SELECT channel, date::date, date_granularity, campaign_name, COALESCE(SUM(spend),0) as spend, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(clicks),0) as clicks, 
         COALESCE(SUM(add_to_cart),0) as add_to_cart, COALESCE(SUM(purchases),0) as paid_purchases, COALESCE(SUM(revenue),0) as paid_revenue, 
-        0 as sho_purchases, 0 as sho_revenue, 0 as sho_first_orders, 0 as sho_first_order_revenue, 0 as sho_repeat_orders, 0 as sho_repeat_order_revenue
+        0 as sho_purchases, 0 as sho_revenue, 0 as sho_first_orders, 0 as sho_first_order_revenue, 0 as sho_repeat_orders, 0 as sho_repeat_order_revenue, 0 as sho_net_revenue
     FROM
         (SELECT 'Meta' as channel, date, date_granularity, campaign_name,
             spend, impressions, link_clicks as clicks, add_to_cart, purchases, revenue
@@ -22,7 +22,8 @@ WITH paid_data as
         0 as spend, 0 as impressions, 0 as clicks, 0 as add_to_cart, 0 as paid_purchases, 0 as paid_revenue,
         COALESCE(SUM(orders),0) as sho_purchases, COALESCE(SUM(gross_sales),0) as sho_revenue, 
         COALESCE(SUM(first_orders),0) as sho_first_orders, COALESCE(SUM(first_order_gross_sales),0) as sho_first_order_revenue, 
-        COALESCE(SUM(repeat_orders),0) as sho_repeat_orders, COALESCE(SUM(repeat_order_gross_sales),0) as sho_repeat_order_revenue
+        COALESCE(SUM(repeat_orders),0) as sho_repeat_orders, COALESCE(SUM(repeat_order_gross_sales),0) as sho_repeat_order_revenue,
+        COALESCE(SUM(net_sales),0) as sho_net_revenue
     FROM {{ source('reporting','shopify_sales') }}
     GROUP BY channel, date, date_granularity, campaign_name)
     
